8a4a780d00e6706c9b650f49734423767bf9e349
d1son <dson120@gmail.com>
2016-02-15 12:24:32 -0500

    fixed the scehma file, and updated the connection.js file

--- db/config/connection.js
+++ db/config/connection.js
@@ -1,14 +1,19 @@
 //set up the code to connect Node to MySQL
-
-var express    = require('express');
-var bodyParser = require('body-parser');
-var mysql      = require('mysql');
-var app 			 = require("express");
-
+var mysql = require('mysql');
 
 var connection = mysql.createConnection({
   host     : "localhost",
   user     : "root",
   database : "burgers_db"
+  port     : 3306
 });
 
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,11 @@
 CREATE DATABASE burgers_db;
 
-CRETATE TABLE burgers (
-	id int AUTO_INCREMENT NOT NULL,
-	burger_name VARCHAR(50),
+USE burgers_db;
+
+CREATE TABLE burgers (
+	ID int AUTO_INCREMENT NOT NULL,
+	burger_name VARCHAR(255) NOT NULL,
 	devoured BOOLEAN,
-	date TIMESTAMP
+	date TIMESTAMP,
+	PRIMARY KEY(ID)
 );
