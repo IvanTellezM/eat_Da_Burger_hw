8c229894f49cc59ccfc14385d7db355c833138eb
d1son <dson120@gmail.com>
2016-02-15 12:45:20 -0500

    created mysql queries in the orm.js file, imported connection.js

--- db/config/orm.js
+++ db/config/orm.js
@@ -1,4 +1,28 @@
 //import connection.js into this file
-
+var connection = require('../config/connection.js');
 
 //cretate the code that will execute MySql commands
+var orm = {
+	//this function will select all burgers from the table which is a user input
+	selectAllBurgers: function(tableInput, cb) {
+		var s = "SELECT * FROM " + tableInput + ";";
+		connection.query(s, function(err, result) {
+			if (err) throw err;
+			cb(result);
+		});
+	}
+	addBurger: function(tableInput, burgerInput, cb) {
+		var a = "INSERT INTO " + tableInput + " (burger_name) VALUES (?)";
+		connection.query(a, [burgerInput], function(err, result) {
+			if (err) throw err;
+			cb(result);
+		});
+	}
+	devourBurger: function(tableInput, idInput, cb) {
+		var d = "UPDATE" + tableInput + " SET devoured = 1 WHERE id = ?";
+		connection.query(d, [idInput], function(err, result) {
+			if (err) throw err;
+			cb(result);
+		});
+	}
+};
