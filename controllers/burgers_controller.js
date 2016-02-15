2762c3561edf9e66edf466cd0f44931d2f856710
d1son <dson120@gmail.com>
2016-02-15 14:26:19 -0500

    added the burgers_controller.js and set up the routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,25 @@
 var express = require("express");
-var router = require("router");
-var burger = require("..models/burger.js")
+var router = express.Router();
+var burger = require("..models/burger.js");
+
+router.get('/', function(req,res) {
+  burger.findAll(function(burger_data){
+    res.render('index', {burger_data});
+  });
+});
+
+router.post('/create', function(req, res) {
+  burger.addBurger(req.body.burger_name, function(result){
+    console.log(result);
+    res.redirect('/');
+  });
+});
+
+router.post('/update', function(req, res){
+  console.log(req.body.id);
+  burger.devourBurger(req.body.id, function(result){
+    console.log(result);
+    res.redirect('/');
+  });
+});
+
